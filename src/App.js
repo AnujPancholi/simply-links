15ec46f816d7ea0c28c7b9979072b4c79409899e
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-01-21 16:17:09 +0530

    feat: use URL as a prop in LinkBox component

--- src/App.js
+++ src/App.js
@@ -11,13 +11,15 @@ const TitlePicture = () => {
 }
 
 const LinkBox = ({
-  text
+  text,
+  url
 }) => {
 
   text = text || "Link Text"
+  url = url || "http://www.google.com"
 
   return (<div className="link-container flex-p-hor flex-p-center-absolute">
-    <a className="link-text" href="http://www.google.com" target="_blank" rel="noopener noreferrer">{text}</a>
+    <a className="link-text" href={url} target="_blank" rel="noopener noreferrer">{text}</a>
 
   </div>)
 }
