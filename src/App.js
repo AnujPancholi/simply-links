00027fec9b66b8272f7d305c28fbf8b5f5d6c6c9
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-01-21 14:27:14 +0530

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './css/flex.css'
+
+
+const TitlePicture = () => {
+
+  return (<div>
+    <img className="title-pic-img" src="https://avatars2.githubusercontent.com/u/25117330?v=4" alt="text" />
+  </div>)
+}
+
+const LinkBox = ({
+  text
+}) => {
+
+  text = text || "Link Text"
+
+  return (<div className="link-container flex-p-hor flex-p-center-absolute">
+    <a className="link-item" href="http://www.google.com" target="_blank" rel="noopener noreferrer">{text}</a>
+
+  </div>)
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-p-ver flex-p-center-absolute">
+      <div className="main-conatainer flex-p-ver flex-p-align-center">
+        <div className="flex-p-hor flex-p-center-absolute">
+          <TitlePicture />
+       </div>
+       <div>
+        <LinkBox />
+        <LinkBox text="Link sdlfjksldifjsoifnsoijfsoidnfoejnf sldkjfsl"/>
+         </div>
+      </div>
     </div>
   );
 }
