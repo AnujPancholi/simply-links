ca868edbb3d353b8f4daef82e9aee6c1fc5ec8f2
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-01-23 11:37:32 +0530

    refactor: changes in temp data

--- src/services/userData.js
+++ src/services/userData.js
@@ -1,38 +1,38 @@
-const USER_DATA_TEMP = {
-  basic_info: {
-    name: 'Anuj Pancholi',
-    pic_url: 'https://avatars2.githubusercontent.com/u/25117330?v=4',
-  },
-  links: [
-    {
-      text: 'Github',
-      url: 'https://github.com/AnujPancholi',
-    },
-    {
-      text: 'Stackoverflow',
-      url: 'https://stackoverflow.com/users/6718353/anuj-pancholi',
-    },
-    {
-      text: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/anuj-pancholi-830056104/',
-    },
-    {
-      text: 'AngelList',
-      url: 'https://angel.co/u/anuj-pancholi',
-    },
-    {
-      text: 'Dev.to',
-      url: 'https://dev.to/anujpancholi',
-    },
-    {
-      text: 'Medium',
-      url: 'https://medium.com/@anuj.pancholi.1',
-    },
-  ],
-};
 
 export const getUserData = () => {
   return new Promise((resolve, reject) => {
+    const USER_DATA_TEMP = {
+      basic_info: {
+        name: 'Anuj Pancholi',
+        pic_url: 'https://avatars2.githubusercontent.com/u/25117330?v=4',
+      },
+      links: [
+        {
+          text: 'Github',
+          url: 'https://github.com/AnujPancholi',
+        },
+        {
+          text: 'Stackoverflow',
+          url: 'https://stackoverflow.com/users/6718353/anuj-pancholi',
+        },
+        {
+          text: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/anuj-pancholi-830056104/',
+        },
+        {
+          text: 'AngelList',
+          url: 'https://angel.co/u/anuj-pancholi',
+        },
+        {
+          text: 'Dev.to',
+          url: 'https://dev.to/anujpancholi',
+        },
+        {
+          text: 'Medium',
+          url: 'https://medium.com/@anuj.pancholi.1',
+        },
+      ],
+    };
     // making this a promise as this may be an API call
     resolve(USER_DATA_TEMP);
   });
